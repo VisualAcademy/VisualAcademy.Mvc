948750686ab4ab7981d904419b69df0f5de3180f
WebCampKR <webcamp@outlook.kr>
2019-05-01 00:26:29 +0900

    ASP.NET Core Identity UI 재 정의를 위한 스캐폴드 항목 추가 기능 적용

---
+++ VisualAcademy/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VisualAcademy.Data;
+
+[assembly: HostingStartup(typeof(VisualAcademy.Areas.Identity.IdentityHostingStartup))]
+namespace VisualAcademy.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
